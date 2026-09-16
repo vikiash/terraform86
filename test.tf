d35958c6db674470fb228c906732bc5658ef4a20
Vivek <36809735+vikiash@users.noreply.github.com>
2026-09-16 11:52:26 +0530

    Line25changed

--- test.tf
+++ test.tf
@@ -22,7 +22,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Globally unique name for the S3 bucket"
   type        = string
-  default     = 'my-unique-bucket-name-12345'
+  default     = "my-unique-bucket-name-12345"
 }
 
 variable "environment" {
